13c97952e993d43a24e17524e117eee8ce4eca8a
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-02-25 22:17:18 +0100

    vuela

--- src/app/services/users_service.go
+++ src/app/services/users_service.go
@@ -31,170 +31,40 @@ func (r *UsersService) GetUsers() ([]model.UserDTO, error) {
 		return nil, aggErr
 	}
 
+	pool := worker.New().
+		WithMaxGoroutines(3)
+
 	var users []model.UserDTO
-
-	parent := worker.New().WithMaxGoroutines(3)
-	parent.Go(func() {
-		child := worker.New().WithMaxGoroutines(2)
-		uChan := make(chan worker.Task[*model.UserResponse], len(userResponses))
-		child.Go(func() {
-			for i := 0; i < len(userResponses); i++ {
-				userTask := <-uChan
-				if userTask.Err != nil {
-					aggErr = multierr.Append(aggErr, userTask.Err)
-					return
-				}
-
-				userDTO := &model.UserDTO{
-					ID:     userTask.Result.ID,
-					Name:   userTask.Result.Name,
-					Email:  userTask.Result.Email,
-					Gender: userTask.Result.Gender,
-					Status: userTask.Result.Status,
-				}
-
-				users = append(users, *userDTO)
-			}
-		})
-
-		child.Go(func() {
-			worker.ForEach(userResponses, func(userResponse *model.UserResponse) {
-				uChan <- worker.ToTask[*model.UserResponse](func() (*model.UserResponse, error) {
-					return r.userClient.GetUser(userResponse.ID)
-				})
-			}, runtime.NumCPU()-1)
-		})
-
-		child.Wait()
+	pool.Go(func() {
+		usersDTO, err := r.getUsers(userResponses)
+		if err != nil {
+			aggErr = multierr.Append(aggErr, err)
+			return
+		}
+		users = append(users, usersDTO...)
 	})
 
 	var posts []model.PostDTO
-	parent.Go(func() {
-		child := worker.New().WithMaxGoroutines(2)
-		pChan := make(chan worker.Task[[]model.PostResponse], len(userResponses))
-		child.Go(func() {
-			for i := 0; i < len(userResponses); i++ {
-				postTask := <-pChan
-				if postTask.Err != nil {
-					aggErr = multierr.Append(aggErr, postTask.Err)
-					return
-				}
-
-				for k := 0; k < len(postTask.Result); k++ {
-					postDTO := model.PostDTO{
-						Comments: make([]model.CommentDTO, 0),
-						ID:       postTask.Result[k].ID,
-						UserID:   postTask.Result[k].UserID,
-						Title:    postTask.Result[k].Title,
-						Body:     postTask.Result[k].Body,
-					}
-					posts = append(posts, postDTO)
-				}
-			}
-
-			if aggErr != nil {
-				return
-			}
-
-			var comments []model.CommentDTO
-			cChan := make(chan worker.Task[[]model.CommentResponse], len(posts))
-			cChild := worker.New()
-			cChild.Go(func() {
-				for i := 0; i < len(posts); i++ {
-					commentTask := <-cChan
-					if commentTask.Err != nil {
-						aggErr = multierr.Append(aggErr, commentTask.Err)
-						return
-					}
-					for k := 0; k < len(commentTask.Result); k++ {
-						commentDTO := &model.CommentDTO{
-							ID:     commentTask.Result[k].ID,
-							PostID: commentTask.Result[k].PostID,
-							Name:   commentTask.Result[k].Name,
-							Email:  commentTask.Result[k].Email,
-							Body:   commentTask.Result[k].Body,
-						}
-						comments = append(comments, *commentDTO)
-					}
-				}
-			})
-
-			cChild.Go(func() {
-				worker.ForEach(posts, func(postDTO *model.PostDTO) {
-					cChan <- worker.ToTask[[]model.CommentResponse](func() ([]model.CommentResponse, error) {
-						return r.userClient.GetComments(postDTO.ID)
-					})
-				}, runtime.NumCPU()-1)
-			})
-
-			cChild.Wait()
-
-			if aggErr != nil {
-				return
-			}
-
-			for i := 0; i < len(posts); i++ {
-				post := &posts[i]
-				for k := 0; k < len(comments); k++ {
-					if post.ID == comments[k].PostID {
-						post.Comments = append(post.Comments, comments[k])
-					}
-				}
-
-				slices.SortFunc(post.Comments, func(a, b model.CommentDTO) int {
-					return a.ID - b.ID
-				})
-			}
-		})
-
-		child.Go(func() {
-			worker.ForEach(userResponses, func(userResponse *model.UserResponse) {
-				pChan <- worker.ToTask[[]model.PostResponse](func() ([]model.PostResponse, error) {
-					return r.userClient.GetPosts(userResponse.ID)
-				})
-			}, runtime.NumCPU()-1)
-		})
-
-		child.Wait()
+	pool.Go(func() {
+		postsDTO, err := r.getPosts(userResponses)
+		if err != nil {
+			aggErr = multierr.Append(aggErr, err)
+			return
+		}
+		posts = append(posts, postsDTO...)
 	})
 
 	var todos []model.TodoDTO
-	parent.Go(func() {
-		tChan := make(chan worker.Task[[]model.TodoResponse], len(userResponses))
-		child := worker.New().WithMaxGoroutines(2)
-		child.Go(func() {
-			for i := 0; i < len(userResponses); i++ {
-				todoTask := <-tChan
-				if todoTask.Err != nil {
-					aggErr = multierr.Append(aggErr, todoTask.Err)
-					return
-				}
-				for k := 0; k < len(todoTask.Result); k++ {
-					todoDTO := model.TodoDTO{
-						ID:     todoTask.Result[k].ID,
-						UserID: todoTask.Result[k].UserID,
-						Title:  todoTask.Result[k].Title,
-						DueOn:  todoTask.Result[k].DueOn,
-						Status: todoTask.Result[k].Status,
-					}
-
-					todos = append(todos, todoDTO)
-				}
-			}
-		})
-
-		child.Go(func() {
-			worker.ForEach(userResponses, func(userResponse *model.UserResponse) {
-				tChan <- worker.ToTask[[]model.TodoResponse](func() ([]model.TodoResponse, error) {
-					return r.userClient.GetTodos(userResponse.ID)
-				})
-			}, runtime.NumCPU()-1)
-		})
-
-		child.Wait()
+	pool.Go(func() {
+		todosDTO, err := r.getTodos(userResponses)
+		if err != nil {
+			aggErr = multierr.Append(aggErr, err)
+			return
+		}
+		todos = append(todos, todosDTO...)
 	})
 
-	parent.Wait()
+	pool.Wait()
 
 	if aggErr != nil {
 		return nil, aggErr
@@ -225,4 +95,187 @@ func (r *UsersService) GetUsers() ([]model.UserDTO, error) {
 	})
 
 	return users, nil
+}
+
+func (r *UsersService) getPosts(userResponses []model.UserResponse) ([]model.PostDTO, error) {
+	var (
+		posts  []model.PostDTO
+		aggErr error
+	)
+
+	rChan := make(chan worker.Task[[]model.PostResponse], len(userResponses))
+
+	pool := worker.New().WithMaxGoroutines(2)
+	pool.Go(func() {
+		for i := 0; i < len(userResponses); i++ {
+			postTask := <-rChan
+			if postTask.Err != nil {
+				aggErr = multierr.Append(aggErr, postTask.Err)
+				return
+			}
+
+			for k := 0; k < len(postTask.Result); k++ {
+				postDTO := model.PostDTO{
+					Comments: make([]model.CommentDTO, 0),
+					ID:       postTask.Result[k].ID,
+					UserID:   postTask.Result[k].UserID,
+					Title:    postTask.Result[k].Title,
+					Body:     postTask.Result[k].Body,
+				}
+				posts = append(posts, postDTO)
+			}
+		}
+
+		if aggErr != nil {
+			return
+		}
+
+		var comments []model.CommentDTO
+		cChan := make(chan worker.Task[[]model.CommentResponse], len(posts))
+		child := worker.New()
+
+		child.Go(func() {
+			for i := 0; i < len(posts); i++ {
+				commentTask := <-cChan
+				if commentTask.Err != nil {
+					aggErr = multierr.Append(aggErr, commentTask.Err)
+					return
+				}
+				for k := 0; k < len(commentTask.Result); k++ {
+					commentDTO := &model.CommentDTO{
+						ID:     commentTask.Result[k].ID,
+						PostID: commentTask.Result[k].PostID,
+						Name:   commentTask.Result[k].Name,
+						Email:  commentTask.Result[k].Email,
+						Body:   commentTask.Result[k].Body,
+					}
+					comments = append(comments, *commentDTO)
+				}
+			}
+		})
+
+		child.Go(func() {
+			worker.ForEach(posts, func(postDTO *model.PostDTO) {
+				cChan <- worker.ToTask[[]model.CommentResponse](func() ([]model.CommentResponse, error) {
+					return r.userClient.GetComments(postDTO.ID)
+				})
+			}, runtime.NumCPU()-1)
+		})
+
+		child.Wait()
+
+		if aggErr != nil {
+			return
+		}
+
+		for i := 0; i < len(posts); i++ {
+			post := &posts[i]
+			for k := 0; k < len(comments); k++ {
+				if post.ID == comments[k].PostID {
+					post.Comments = append(post.Comments, comments[k])
+				}
+			}
+
+			slices.SortFunc(post.Comments, func(a, b model.CommentDTO) int {
+				return a.ID - b.ID
+			})
+		}
+	})
+
+	pool.Go(func() {
+		worker.ForEach(userResponses, func(userResponse *model.UserResponse) {
+			rChan <- worker.ToTask[[]model.PostResponse](func() ([]model.PostResponse, error) {
+				return r.userClient.GetPosts(userResponse.ID)
+			})
+		}, runtime.NumCPU()-1)
+	})
+
+	pool.Wait()
+
+	return posts, aggErr
+}
+
+func (r *UsersService) getTodos(userResponses []model.UserResponse) ([]model.TodoDTO, error) {
+	var (
+		todos  []model.TodoDTO
+		aggErr error
+	)
+
+	rChan := make(chan worker.Task[[]model.TodoResponse], len(userResponses))
+
+	pool := worker.New().WithMaxGoroutines(2)
+	pool.Go(func() {
+		for i := 0; i < len(userResponses); i++ {
+			todoTask := <-rChan
+			if todoTask.Err != nil {
+				aggErr = multierr.Append(aggErr, todoTask.Err)
+				return
+			}
+			for k := 0; k < len(todoTask.Result); k++ {
+				todoDTO := model.TodoDTO{
+					ID:     todoTask.Result[k].ID,
+					UserID: todoTask.Result[k].UserID,
+					Title:  todoTask.Result[k].Title,
+					DueOn:  todoTask.Result[k].DueOn,
+					Status: todoTask.Result[k].Status,
+				}
+
+				todos = append(todos, todoDTO)
+			}
+		}
+	})
+
+	pool.Go(func() {
+		worker.ForEach(userResponses, func(userResponse *model.UserResponse) {
+			rChan <- worker.ToTask[[]model.TodoResponse](func() ([]model.TodoResponse, error) {
+				return r.userClient.GetTodos(userResponse.ID)
+			})
+		}, runtime.NumCPU()-1)
+	})
+
+	pool.Wait()
+
+	return todos, aggErr
+}
+
+func (r *UsersService) getUsers(userResponses []model.UserResponse) ([]model.UserDTO, error) {
+	var (
+		users  []model.UserDTO
+		aggErr error
+	)
+
+	rChan := make(chan worker.Task[*model.UserResponse], len(userResponses))
+	pool := worker.New().WithMaxGoroutines(2)
+
+	pool.Go(func() {
+		for i := 0; i < len(userResponses); i++ {
+			userTask := <-rChan
+			if userTask.Err != nil {
+				aggErr = multierr.Append(aggErr, userTask.Err)
+				continue
+			}
+
+			userDTO := &model.UserDTO{
+				ID:     userTask.Result.ID,
+				Name:   userTask.Result.Name,
+				Email:  userTask.Result.Email,
+				Gender: userTask.Result.Gender,
+				Status: userTask.Result.Status,
+			}
+
+			users = append(users, *userDTO)
+		}
+	})
+
+	pool.Go(func() {
+		worker.ForEach(userResponses, func(userResponse *model.UserResponse) {
+			rChan <- worker.ToTask[*model.UserResponse](func() (*model.UserResponse, error) {
+				return r.userClient.GetUser(userResponse.ID)
+			})
+		}, runtime.NumCPU()-1)
+	})
+
+	pool.Wait()
+
+	return users, aggErr
 }
